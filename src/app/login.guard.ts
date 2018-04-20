9d61861fa205cd131afa949624cdfe508c66397f
Liran Peleg <pharoah29@gmail.com>
2018-04-20 19:50:47 +0300

    login guard

--- src/app/login.guard.ts
+++ src/app/login.guard.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { SharedService } from './services/shared.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
 
-  constructor(private shared: SharedService, private router: Router) { }
+  constructor(private shared: SharedService, private router: Router,private firebaseAuth: AngularFireAuth ) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
+        debugger;
     let user = this.shared.currentUser;
-
+      
     if (!user) {
-debugger;
+
       this.router.navigate(['login']);
     }
 
--- src/app/services/repo.service.ts
+++ src/app/services/repo.service.ts
@@ -7,32 +7,30 @@ import { Repo } from '../models/repo';
 @Injectable()
 export class RepoService {
 
-  private EndPoint: string = "https://api.github.com/search/repositories?q="; 
+  private EndPoint: string = "https://api.github.com/search/repositories?q=";
 
   private bookmark: Repo[] = [];
-  
-  constructor( private http: HttpClient) { }
 
-  getList(serachText: string) : Observable<any>
-  {      return this.http.get(this.EndPoint + serachText);
-      
+  constructor(private http: HttpClient) { }
+
+  getList(serachText: string): Observable<any> {
+    return this.http.get(this.EndPoint + serachText);
+
   }
 
-  getProducts() : Observable<any>
-  {      return this.http.get("http://localhost:60666/api/products/");
-      
+  getProducts(): Observable<any> {
+    return this.http.get("http://localhost:60666/api/products/");
+
   }
 
+  toStorage(item: Repo) {
 
-
-  toStorage(item: Repo){
-
-    if(this.bookmark.indexOf(item) == -1) this.bookmark.push(item);
-    debugger;
-    sessionStorage.setItem("github.gallery.bookmarks",JSON.stringify(this.bookmark));
+    if (this.bookmark.indexOf(item) == -1) this.bookmark.push(item);
+    
+    sessionStorage.setItem("github.gallery.bookmarks", JSON.stringify(this.bookmark));
   }
 
-  fromStorage<T>(){
+  fromStorage<T>() {
     return JSON.parse(sessionStorage.getItem("github.gallery.bookmarks")) as T;
   }
 
