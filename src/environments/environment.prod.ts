55f9a195f874156b14ada971d2c4a137ffe8d8c2
pharoah29 <pharoah29@gmail.com>
2018-04-21 15:00:37 +0300

    update: firebase page bootstrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAPNc6C0NaqExQiFgy2SDKtwIGvH6iQGi4",
+    authDomain: "database-test-28cdf.firebaseapp.com",
+    databaseURL: "https://database-test-28cdf.firebaseio.com",
+    projectId: "database-test-28cdf",
+    storageBucket: "",
+    messagingSenderId: "120841249355"
+  }
 };
