ff21a62cd640c4c0ac3d1e2b885428288250e2b9
pharoah29 <pharoah29@gmail.com>
2018-04-29 14:47:13 +0300

    update: move all routing related files from app.module into app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, CanActivate, RouterModule } from '@angular/router';
 import { FirebaseDatabaseComponent } from './components/firebase-database/firebase-database.component';
 import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
 import { AppComponent } from './app.component';
-import { NgModule } from '@angular/core';
-import { Routes, CanActivate, RouterModule } from '@angular/router';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginGuard } from './login.guard';
+
 
 const routes: Routes = [
 
@@ -26,7 +30,20 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{onSameUrlNavigation:'reload'})],
-  exports: [RouterModule]
+  declarations:[    
+    GalleryComponent,
+    BookmarksComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent],
+
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot(routes,{onSameUrlNavigation:'reload'})],
+
+  exports: [RouterModule],
+  
+  providers:[LoginGuard]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { environment } from './../environments/environment';
 
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,28 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
+
 import { RepoService } from './services/repo.service';
 import { SharedService } from './services/shared.service';
-import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
 
-
-
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { LoginGuard } from './login.guard';
 import { AuthService } from './services/auth.service';
 import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent,
-    BookmarksComponent,
-    LoginComponent,
-    HomeComponent,
-    PageNotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -38,7 +26,7 @@ import { CoreModule } from './core.module';
     FormsModule,
     CoreModule
   ],
-  providers: [RepoService, SharedService, LoginGuard, AuthService],
+  providers: [RepoService, SharedService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
